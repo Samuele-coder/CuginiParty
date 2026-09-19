b2192296a0adb564db5bc4d23d2ae2e907e920d5
Samuele-coder <spadaro.samuele.21@gmail.com>
2026-09-19 08:24:29 +0000

    Rimuovi tutti i file

--- polizia_ladri_icon.sql
+++
@@ -1,8 +0,0 @@
--- CuginiParty / Polizia e Ladri - icona catalogo
--- Patch additiva: aggiorna solo il vecchio segnaposto a pallino.
--- Eseguire nel Supabase SQL Editor dopo aver pubblicato assets/polizia-ladri-icon.svg.
-
-update public.site_games
-set icon = 'assets/polizia-ladri-icon.svg'
-where game_id = 'polizia-ladri'
-  and (icon is null or lower(btrim(icon)) in ('', 'pl') or btrim(icon) in ('◉', '●'));
